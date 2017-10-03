8c1961f7c3e1d349ed21102c4d61622be786822f
alan guegan <alan guegan>
2017-10-03 15:04:31 +0200

    add back fiche besoin

--- app/src/main/java/com/gfi/appcommercial/activity/FicheBesoinActivity.java
+++ app/src/main/java/com/gfi/appcommercial/activity/FicheBesoinActivity.java
@@ -1,8 +1,20 @@
 package com.gfi.appcommercial.activity;
 
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.gfi.appcommercial.R;
+
 /**
  * Created by ASUS on 03/10/2017.
  */
 
-public class FicheBesoinActivity {
+public class FicheBesoinActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fiche_besoin);
+    }
+
+
 }
