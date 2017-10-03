54f2ffdfe4d126ea927d6bcd6ad5bfe04011be35
Straub <robin@troopers.email>
2017-10-03 13:31:22 +0200

    rename list activity

--- app/src/main/java/com/gfi/appcommercial/activity/ListActivity.java
+++ app/src/main/java/com/gfi/appcommercial/activity/ListActivity.java
@@ -8,7 +8,7 @@ import android.widget.Spinner;
 
 import com.gfi.appcommercial.R;
 
-public class list extends AppCompatActivity {
+public class ListActivity extends AppCompatActivity {
     private ListView mListView;
     private String[] prenoms = new String[]{
             "Antoine", "Benoit", "Cyril", "David", "Eloise", "Florent",
@@ -27,14 +27,14 @@ public class list extends AppCompatActivity {
         // Create an ArrayAdapter using the string array and a default spinner layout
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
                 R.array.tri, android.R.layout.simple_spinner_item);
-        // Specify the layout to use when the list of choices appears
+        // Specify the layout to use when the ListActivity of choices appears
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         // Apply the adapter to the spinner
         spinner.setAdapter(adapter);
 
         //lisyView init
         mListView = (ListView) findViewById(R.id.listView);
-        final ArrayAdapter<String> dapter = new ArrayAdapter<String>(list.this,
+        final ArrayAdapter<String> dapter = new ArrayAdapter<String>(ListActivity.this,
                 android.R.layout.simple_list_item_1, prenoms);
         mListView.setAdapter(dapter);
 
